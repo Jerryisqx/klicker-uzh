69818b5f5cb99de254e31012e29ccc83491d9e3b
kyuchiang <kingyeolchiang@gmail.com>
2024-11-16 13:28:01 +0100

    merge successfuly

--- apps/ai/main.py
+++ apps/ai/main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 # Initialize Prisma client
 db = Prisma()
-os.environ['DATABASE_URL'] = 'postgresql://klicker:klicker@host.docker.internal:5432/klicker'
+os.environ['DATABASE_URL'] = 'postgresql://klicker:klicker@192.168.254.170:5432/klicker'
 db.connect()
 # CORS 配置：允许来自前端的跨域请求
 app.add_middleware(
